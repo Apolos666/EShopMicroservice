fa42ed6808307bebf478f6af31b9c6dcd773a258
Apolos666 <quangprox121@gmail.com>
2024-08-18 17:23:57 +0700

    feat: 141. Test - Docker-Compose Basket.API microservices with PostgresDb and Redis

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,19 @@
+﻿global using Basket.API.Models;
+global using BuildingBlocks.CQRS;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions;
+global using Marten;
+global using Basket.API.Exceptions;
+global using Weasel.Core;
+global using Basket.API.Data;
+global using BuildingBlocks.Exceptions.Handler;
+global using System.Text.Json;
+global using Microsoft.Extensions.Caching.Distributed;
+global using HealthChecks.UI.Client;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+
