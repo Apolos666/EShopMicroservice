70fd3451419095aab61d7bf4469a24c81190f47d
Apolos666 <quangprox121@gmail.com>
2024-08-22 14:01:37 +0700

    200

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        //// Add Carter
+        //services.AddCarter();
+        //// Add HealthChecks
+        //services.AddHealthChecks();
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app)
+    {
+        //app.UseHealthChecks("/health");
+        //app.UseRouting();
+        //app.UseEndpoints(endpoints =>
+        //{
+        //    endpoints.MapCarter();
+        //});
+        return app;
+    }
+}
